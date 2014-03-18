ae9f25ba7bab6075e0352f03c35a2cfafdb0a8ff
Montri <montri2025@gmail.com>
2014-03-18 12:01:54 +0700

    Fix new date and Add suite test

--- PersisterMySQLTest.php
+++ PersisterMySQLTest.php
@@ -4,7 +4,7 @@ require_once "ApiLog.php";
 class PersisterMySQLTest extends PHPUnit_Framework_TestCase {
   function testPersisterMySQLShouldReturnTrue() {
     $persisterMySQL = new PersisterMySQL();
-    $this->assertTrue($persisterMySQL->persistLog(new ApiLog(uniqid(), "/api/v4/authentication", new date('Y-m-d H:i:s'), "127.0.0.1")));
+    $this->assertTrue($persisterMySQL->persistLog(new ApiLog(uniqid(), "/api/v4/authentication",date('Y-m-d H:i:s'), "127.0.0.1")));
   }
   function testPersisterMySQLShouldReturn() {
     $persisterMySQL = new PersisterMySQL();
